845c701e5b78fe70a47d8964d87e8d14537b4e2f
TheDonKing <starspersona@gmail.com>
2016-09-09 18:59:29 +0300

    Добавление анимации при hovere

--- app/js/common.js
+++ app/js/common.js
@@ -36,7 +36,8 @@ $(function() {
         items:1,
         margin:30,
         stagePadding:30,
-        smartSpeed:450
+        smartSpeed:450,
+        nav:true
     });
 
     //E-mail Ajax форма
@@ -73,4 +74,30 @@ $(function() {
     //Documentation & Example: https://github.com/inuyaksa/jquery.nicescroll
     $("html").niceScroll();
 
+    function fix_size() {
+
+        var images = $('.effect-julia');
+        images.each(setsize);
+
+        function setsize() {
+            var img = $(this),
+                img_dom = img.get(0),
+                container = img.parents('.img-container');
+            if (img_dom.complete) {
+                resize();
+            } else img.one('load', resize);
+
+            function resize() {
+                if ((container.width() / container.height()) < (img_dom.width / img_dom.height)) {
+                    img.width('100%');
+                    img.height('auto');
+                    return;
+                }
+                img.height('100%');
+                img.width('auto');
+            }
+        }
+    }
+    $(window).on('resize', fix_size);
+    fix_size();
 });
